731bc8ed4a099b70ab072c2ad03cec64daa7fa19
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-09 19:18:41 +0300

    Route the pages and display their respective views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
+// All listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest listings' ,
+        'listings' => Listing::all(),
+    ]);
+});
+
+Route::get('/listings/{id}', function($id) {
+    return view('listing', [
+        'listing' => Listing::find($id),
+    ]);
 });
