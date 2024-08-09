aafe3a817c2516316ceed26a2559dac368be892b
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-09 20:30:48 +0300

    Add laravel color to the tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                laravel: "#ef3b2d",
+            },
+        },
     },
     plugins: [],
 };
