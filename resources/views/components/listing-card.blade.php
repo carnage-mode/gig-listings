a1600f8a18f7be1af01d7fe94b3e28e005c249fd
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-11 19:08:10 +0300

    Add a tags component

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -5,6 +5,7 @@
         <div>
             <h2 class="text-2xl"><a href="listings/{{$listing->id}}">{{$listing->title}}</a></h2>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+            <x-listing-tags :tagsCSV="$listing->tags"/>
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i>
                 {{$listing->location}}
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -23,6 +23,7 @@
 
                 <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
                 <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+                <x-listing-tags :tagsCSV="$listing->tags"/>
                 <div class="text-lg my-4">
                     <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
                 </div>
