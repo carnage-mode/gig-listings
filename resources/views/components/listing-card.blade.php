184e2ba134beffcb7a3059d2f783e00e22095212
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-11 19:05:51 +0300

    Move the listing card styling into a component

---
+++ resources/views/components/listing-card.blade.php
@@ -0,0 +1,14 @@
+@props(['listing'])
+<x-card>
+    <div class="flex">
+        <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
+        <div>
+            <h2 class="text-2xl"><a href="listings/{{$listing->id}}">{{$listing->title}}</a></h2>
+            <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i>
+                {{$listing->location}}
+            </div>
+        </div>
+    </div>
+</x-card>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -9,20 +9,7 @@
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 @unless (count($listings) == 0)
 @foreach ($listings as $listing)
-    <div class="bg-gray-50 border border-gray-200 rounded p-6">
-        <div class="flex">
-            <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
-            <div>
-                <h2 class="text-2xl"><a href="listings/{{$listing->id}}">{{$listing->title}}</a></h2>
-                <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-                <!-- <p>{{$listing->description}}</p> -->
-                <div class="text-lg mt-4">
-                    <i class="fa-solid fa-location-dot"></i>
-                    {{$listing->location}}
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-listing-card :listing="$listing"/>
 @endforeach
 @else
     <p>No listings found</p>
