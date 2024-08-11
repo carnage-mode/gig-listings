9ec74da18424469d051bbd4a08d69b7c70258da9
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-11 18:10:57 +0300

    Style the templates using tailwind

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,10 +4,37 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
+        @vite('resources/css/app.css')
     </head>
     <body>
-    <h1>Laragigs</h1>
-    @yield('content')
+        <nav class="flex justify-between items-center mb-4">
+            <a href="/">
+                <img class="w-24" src="{{ asset('/images/logo.png') }}" alt="" class="logo"/>
+            </a>
+            <ul class="flex space-x-6 mr-6 text-lg">
+                <li>
+                    <a href="register.html" class="hover:text-laravel" >
+                        <i class="fa-solid fa-user-plus"></i>
+                        Register
+                    </a>
+                </li>
+                <li>
+                    <a href="login.html" class="hover:text-laravel">
+                        <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                        Login
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+        <main class="pb-24 mb-24">
+            @yield('content')
+        </main>
+
+        <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
+            <p class="ml-2">Copyright &copy; 2024, All Rights reserved</p>
+            <a href="create.html" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a
+        </footer>
 
     </body>
 </html>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,14 +2,29 @@
 
 @section('content')
 
-<h1>{{$heading}}</h1>
+<h1 class="text-center text-2xl font-bold">{{$heading}}</h1>
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 @unless (count($listings) == 0)
 @foreach ($listings as $listing)
-    <h2><a href="listings/{{$listing['id']}}">{{$listing['title']}}</a></h2>
-    <p>{{$listing['description']}}</p>
+    <div class="bg-gray-50 border border-gray-200 rounded p-6">
+        <div class="flex">
+            <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
+            <div>
+                <h2 class="text-2xl"><a href="listings/{{$listing->id}}">{{$listing->title}}</a></h2>
+                <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+                <!-- <p>{{$listing->description}}</p> -->
+                <div class="text-lg mt-4">
+                    <i class="fa-solid fa-location-dot"></i>
+                    {{$listing->location}}
+                </div>
+            </div>
+        </div>
+    </div>
 @endforeach
 @else
     <p>No listings found</p>
 @endunless
+
+</div>
 
 @endsection
