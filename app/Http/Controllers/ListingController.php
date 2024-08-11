3ba887a5ddf6c946f8adf5510830e5608cc94f4d
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-12 01:32:28 +0300

    Enable pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -16,7 +16,7 @@ class ListingController extends Controller
         return view('listings.index', [
             'listings' => Listing::latest()
                 ->filter(request(['tag', 'search']))
-                ->get(),
+                ->paginate(6),
         ]);
     }
 
