027df25723bca5a4ecf1fc0c9ab0767d665e93a0
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-11 20:28:46 +0300

    Enable search functionality

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -40,6 +40,11 @@ class Listing extends Model
     public function scopeFilter($query, array $filters) {
         if($filters['tag'] ?? false) {
             $query->where('tags', 'like', '%' . request('tag') . '%');
+        } elseif ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('description', 'like', '%' . request('search') . '%')
+                ->orWhere('location', 'like', '%' . request('search') . '%')
+                ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
     }
 }
