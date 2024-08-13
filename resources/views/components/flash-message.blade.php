09960efbdf0737f6153ea8840c021dd69475a4f7
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-13 19:19:37 +0300

    Improve flash message styling

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,10 +1,12 @@
 @if (session()->has('message'))
     <div
-        class="fixed top-4 z-20 rounded-full bg-green-500 px-5 py-3 text-white"
+        class="absolute top-1/3 left-10 bg-green-500 text-white py-2 px-10 rounded-lg"
+        x-transition
         x-data="{show: true}"
         x-init="setTimeout(() => show = false, 3000)"
         x-show="show"
     >
+        <i class="fa-solid fa-bell"></i>
         {{session('message')}}
     </div>
 @endif
