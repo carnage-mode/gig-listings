1b26bcfbfbe116b85f0c7ac6a4ef48ec655ded86
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-13 19:33:58 +0300

    Improve flash-message even further

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,12 +1,13 @@
 @if (session()->has('message'))
     <div
-        class="absolute top-1/3 left-10 bg-green-500 text-white py-2 px-10 rounded-lg"
+        class="fixed top-8 z-20 mx-auto bg-black text-white py-2 px-10 rounded-lg"
         x-transition
         x-data="{show: true}"
         x-init="setTimeout(() => show = false, 3000)"
         x-show="show"
     >
         <i class="fa-solid fa-bell"></i>
+        Notification:
         {{session('message')}}
     </div>
 @endif
