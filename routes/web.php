467a707cd27694fd93a1075d0445535afff5af58
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-14 17:39:43 +0300

    Revert "Require auth for certain critical routes"
    This reverts commit a2804280b95012d1fa3ad0d8af379b219afbe1fd.

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,23 @@ use App\Http\Controllers\UserController;
 // All listings
 Route::get('/', [ListingController::class, 'index']);
 
+// Show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store listing data
+Route::post('/listings/', [ListingController::class, 'store']);
+
+// Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Show user registration page
 Route::get('/register', [UserController::class, 'create']);
@@ -19,27 +34,10 @@ Route::get('/register', [UserController::class, 'create']);
 Route::post('/users/', [UserController::class, 'store']);
 
 // Show login form
-Route::get('/login/', [UserController::class, 'login'])->name('login');
+Route::get('/login/', [UserController::class, 'login']);
 
 // Authenticate and login
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
 
-Route::middleware(['auth'])->group(function() {
-    // Show create form
-    Route::get('/listings/create', [ListingController::class, 'create']);
-
-    // Store listing data
-    Route::post('/listings/', [ListingController::class, 'store']);
-
-    // Show edit form
-    Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
-
-    // Update listing
-    Route::put('/listings/{listing}', [ListingController::class, 'update']);
-
-    // Delete listing
-    Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
-
-    // Logout
-    Route::post('/logout', [UserController::class, 'logout']);
-});
+// Logout
+Route::post('/logout', [UserController::class, 'logout']);
